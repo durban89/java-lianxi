6b64b062493763bb29b96d0c96e7e82165921288
durban <durban.zhang@gmail.com>
2021-01-17 22:26:07 +0800

    init Day1

---
+++ Day1/src/com/gowhich/day/Demo.java
@@ -0,0 +1,9 @@
+package com.gowhich.day;
+
+public class Demo {
+	public static void main(String[] args) {
+		MyMath math = new MyMath();
+		int add = math.add(3, 4);
+		System.out.println("result = " + add);
+	} 
+}
---
+++ Day1/src/com/gowhich/day/MyMath.java
@@ -0,0 +1,7 @@
+package com.gowhich.day;
+
+public class MyMath {
+	public int add(int i, int j) {
+		return i + j;
+	}
+}
