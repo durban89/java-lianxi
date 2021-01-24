aa695ecaa9677653567c82a76d37e649b719b0b3
durban.zhang <durban.zhang@gmail.com>
2021-01-24 22:08:53 +0800

    update modify xml parser

--- Day1_03_xmlparser/src/com/gowhich/xmlparser/DomXmlParserTest.java
+++ Day1_03_xmlparser/src/com/gowhich/xmlparser/DomXmlParserTest.java
@@ -1,8 +1,14 @@
 package com.gowhich.xmlparser;
 
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.junit.Test;
 import org.w3c.dom.Document;
@@ -22,7 +28,27 @@ public class DomXmlParserTest {
 		System.out.println(content);
 	}
 	
-	public void domXmlModifyParser() {
+	@Test
+	public void domXmlModifyParser() throws Exception {
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document = documentBuilder.parse("book.xml");
+		NodeList nodeList = document.getElementsByTagName("书名");
+		
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node node = nodeList.item(i);
+			String content = node.getTextContent();
+			System.out.println(content);
+		}
+		
+		Node node = nodeList.item(1);
+		node.setTextContent("书名2-改");
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		Source xmlSource = new DOMSource(document);
+		Result outputTarget = new StreamResult("book.xml");
+		transformer.transform(xmlSource , outputTarget);
 		
 	}
 }
