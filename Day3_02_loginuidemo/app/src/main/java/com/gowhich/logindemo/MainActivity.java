be8af3793e09c137b2312270084b704036073138
root <root@WIN-HOAOHKLHJFN.localdomain>
2021-03-06 23:56:16 +0800

    add user login logical

--- Day3_02_loginuidemo/app/src/main/java/com/gowhich/logindemo/MainActivity.java
+++ Day3_02_loginuidemo/app/src/main/java/com/gowhich/logindemo/MainActivity.java
@@ -3,6 +3,13 @@ package com.gowhich.logindemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText1 = (EditText) findViewById(R.id.editText1);
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        CheckBox checkBox = (CheckBox) findViewById(R.id.login_save_checked);
+        Button loginButton = (Button) findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!checkBox.isChecked()) {
+                    Toast.makeText(MainActivity.this, "请勾选", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                String userName = editText1.getText().toString();
+                String password = editText2.getText().toString();
+
+                if (TextUtils.isEmpty(userName)) {
+                    Toast.makeText(MainActivity.this, "请输入用户名", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(MainActivity.this, "请输入密码", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Log.i("Login", "userName:"+userName);
+                Log.i("Login", "password:"+password);
+            }
+        });
+
     }
 }
